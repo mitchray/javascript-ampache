417a24776b0a70646dfd56ee7ae83ac2d8734caf
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2025-05-24 17:53:28 +1000

    Add method songTags

--- src/songs/index.ts
+++ src/songs/index.ts
@@ -163,6 +163,19 @@ export class Songs extends Base {
   }
 
   /**
+   * Get the full song file tags using VaInfo
+   * This is used to get tags for remote catalogs to allow maximum data to be returned
+   * @remarks MINIMUM_API_VERSION=7.5.0
+   * @param params.filter UID of song to fetch
+   * @see {@link https://ampache.org/api/api-json-methods#song_tags}
+   */
+  songTags(params: { filter: UID }) {
+    let query = "song_tags";
+    query += qs.stringify(params, "&");
+    return this.request<SongResponse>(query); // TODO update reponse
+  }
+
+  /**
    * This takes a URL and returns the song object in question
    * @remarks MINIMUM_API_VERSION=380001
    * @param params.url Full Ampache URL from server
